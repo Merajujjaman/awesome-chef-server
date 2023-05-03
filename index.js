7999e827b984e165d642bebc1988bfb4e69021e8
Merajujjaman <asmmerajujjaman@gmail.com>
2023-05-03 11:56:42 +0600

    cors problem solved

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+app.use(cors())
+
+const data = require('./data/chefData.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/data', (req, res) => {
+    res.json(data)
 })
 
 
